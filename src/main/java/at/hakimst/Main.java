eb90a772ded12f2f220f39fa5f48600a43444899
Lex0007 <alklapeer@tsn.at>
2024-11-28 13:28:04 +0100

    4. Video angesehen und Nachprogrammiert

--- src/main/java/at/hakimst/Main.java
+++ src/main/java/at/hakimst/Main.java
@@ -1,12 +1,17 @@
 package at.hakimst;
 
 import at.hakimst.dataaccess.MysqlDatabaseConnection;
+import at.hakimst.ui.Cli;
 
 import java.sql.Connection;
 import java.sql.SQLException;
 
 public class Main {
     public static void main(String[] args) {
+
+        Cli myCli = new Cli();
+        myCli.start();
+
         try {
             Connection myConnection = MysqlDatabaseConnection.getConnection("jdbc:mysql://localhost:3306/kurssystem", "root", "");
             System.out.println("Verbindung aufgebaut");
